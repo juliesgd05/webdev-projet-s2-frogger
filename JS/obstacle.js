f953df17504e70823add9c207d4fae74bdc3d35e
furtest <paul.retourne@orange.fr>
2024-05-18 00:57:18 +0200

    Edition of the Obstacle class to make it become a mother class for all the obstacle types. Addition of CSS, edition of the HTML and addition of images to prepare for the addition of all the obstacles types. Creation of Log and Car daughter classes.

--- JS/obstacle.js
+++ JS/obstacle.js
@@ -1,30 +1,50 @@
 class Obstacle {
 
-    step = 1;
-    name = "obstacle";
-    image = "Obstacle.png";
 
-    constructor(x, y, dir, step, speed) {
+    constructor(x, y, dir, step, speed, name, image) {
+    	this.name = name;
+    	this.image = image;
         this.x = x;
         this.y = y;
         this.dir = dir;
         this.step = step;
         this.speed = speed;
-        this.target = document.createElement("div");
-        document.getElementsByClassName("rectangle")[0].appendChild(this.target);
-        this.target.className = this.name;
     }
 
+
+    spawnObstacle() {
+        this.target = document.createElement("div");
+        document.getElementsByClassName("rectangle")[0].appendChild(this.target);
+        this.target.className += this.name;
+    }
+   
+    // TODO redo the deplacement so it scales better to different screen sizes and resolutions
     moveObstacle() {
         this.x += this.step;
         this.target.style.left = (this.x + "px");
         this.target.style.top = (this.y + "%");
     }
-
+    
     removeObstacle() {
         this.target.remove();
     }
 
+}
+
+class Log extends Obstacle {
+
+    constructor(x, y, step) {
+	super(x, y, "l", step, 0, "log", "foo");
+	this.spawnObstacle()
+    }
+}
+
+class Car extends Obstacle {
+
+    constructor(x, y, step) {
+	super(x, y, "l", step, 0, "car", "foo");
+	this.spawnObstacle()
+    }
 }
 
 class Obstacles {
@@ -57,7 +77,7 @@ class Obstacles {
 var c = new Obstacles(25);
 for (let i = 0; i < 5; i++) {
     //let a = new Obstacle(i * 100, 60, "left", 1, 25);
-    let a = new Obstacle(i * 100, 60, "left", 0, 0);
+    let a = new Car(i * 100, 60, 0);
     c.addElement(a);
 }
 intervalId = setInterval(function () { c.moveElements(); }, c.speed);
