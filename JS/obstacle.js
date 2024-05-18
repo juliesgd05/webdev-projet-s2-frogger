f14bf0af1a7cb06f94ab4037fa221dd96da6de6b
furtest <paul.retourne@orange.fr>
2024-05-18 17:59:16 +0200

    Advancement on the different types of obstacle and preparation for the spawning of the obstacles.

--- JS/obstacle.js
+++ JS/obstacle.js
@@ -1,26 +1,25 @@
 class Obstacle {
 
 
-    constructor(x, y, dir, step, speed, name, image) {
+    constructor(x, y, dir, speed, name) {
     	this.name = name;
-    	this.image = image;
         this.x = x;
         this.y = y;
         this.dir = dir;
-        this.step = step;
         this.speed = speed;
     }
-
-
-    spawnObstacle() {
+    
+    // Spawn the obstacle as a child of the rectangle where it will live
+    // The element will have his type in the class name in order to render it with CSS
+    spawnObstacle(spawnPlace) {
         this.target = document.createElement("div");
-        document.getElementsByClassName("rectangle")[0].appendChild(this.target);
+        document.getElementById(spawnPlace).appendChild(this.target);
         this.target.className += this.name;
     }
    
     // TODO redo the deplacement so it scales better to different screen sizes and resolutions
     moveObstacle() {
-        this.x += this.step;
+        this.x += this.speed;
         this.target.style.left = (this.x + "px");
         this.target.style.top = (this.y + "%");
     }
@@ -31,31 +30,62 @@ class Obstacle {
 
 }
 
+// For all the following classes :
+// - x, y are the spawn coordinates of the obstacle
+// - speed is the distance the obstacles move between each frame
+// - rank is used when the same type of obstacle need to be on different rows,
+// 	it specifies on which one the obstacle need to spawn
+
 class Log extends Obstacle {
 
-    constructor(x, y, step) {
-	super(x, y, "l", step, 0, "log", "foo");
-	this.spawnObstacle()
+    constructor(x, y, speed, rank) {
+	super(x, y, "r", speed, "log");
+	if(! (rank == 1 || rank == 3 || rank == 5)){ rank = 1; }
+	this.spawnObstacle("water" + rank);
     }
 }
 
-class Car extends Obstacle {
 
-    constructor(x, y, step) {
-	super(x, y, "l", step, 0, "car", "foo");
-	this.spawnObstacle()
+class Turtle extends Obstacle {
+
+    constructor(x, y, speed, rank) {
+	super(x, y, "l", speed, "turtle");
+	if(! (rank == 2 || rank == 4)){ rank = 2; }
+	this.spawnObstacle("water" + rank);
     }
 }
+
+class Truck extends Obstacle {
+
+    constructor(x, y, speed) {
+	super(x, y, "l", speed, "car");
+	this.spawnObstacle("ground2");
+    }
+}
+
+class Car1 extends Obstacle {
+    // level 1 : pretty slow, when a car dispawn a second one respawns a bit after
+    // level 2 : same as level 1 but the cars are faster and they spawn two by two
+    // level 3 : even faster and 3 by 3
+
+
+    constructor(x, y, speed) {
+	super(x, y, "r", speed, "car");
+	this.spawnObstacle("ground3");
+    }
+}
+
 
 class Obstacles {
 
     constructor(speed) {
         this.speed = speed;
         this.content = [];
+	this.start();
     }
 
     addElement(obstacle) {
-        this.content.push(obstacle);
+        this.content.unshift(obstacle);
     }
 
     removeElement() {
@@ -65,23 +95,23 @@ class Obstacles {
     moveElements() {
         for (const obstacle of this.content) {
             obstacle.moveObstacle();
-            if (obstacle.x >= (screen.width) * 0.75) {
+            if (obstacle.x >= getPosition(document.getElementByid("plateauDeJeu")).right) {
                 obstacle.removeObstacle();
                 this.content.pop();
             }
         }
     }
+
+    
 }
 
-
+/*
 var c = new Obstacles(25);
 for (let i = 0; i < 5; i++) {
-    //let a = new Obstacle(i * 100, 60, "left", 1, 25);
-    let a = new Car(i * 100, 60, 0);
+    let a = new Car1(i * 100, 60, 10);
     c.addElement(a);
 }
 intervalId = setInterval(function () { c.moveElements(); }, c.speed);
-
+*/
 document.getElementById("score").innerText = "Score: " + score;
-
 
